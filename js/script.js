9784256304a84a9c4a98a3971f32f4cc0b9ff81b
Maurycy <d.wasilewski@zjednoczenie.com>
2011-12-09 10:45:38 +0100

    ajax file-uploader added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@
 
 */
 
+// optional file-uploader init
+function createUploader(){
+    var uploader = new qq.FileUploader({
+        element: document.getElementById('file-uploader'), // remember to crate node with #file-uploader
+        action: 'jx/file_uploader.php',
+        debug: true // change to false once everything is set
+    });
+}
+
 $(function(){
     console.log('init');
 });
